e87d7d2efc17dacc11f6ae974dd7d92f242efc92
kyumdbot <kyumd001@gmail.com>
2020-08-09 17:50:29 +0800

    add: WaterDispenserAnnotation, WaterDispenserAnnotationView

---
+++ Taipei Maps/Views/WaterDispenserAnnotationView.swift
@@ -0,0 +1,9 @@
+//
+//  WaterDispenserAnnotationView.swift
+//  Taipei Maps
+//
+//  Created by Wei-Cheng Ling on 2020/8/9.
+//  Copyright © 2020 Wei-Cheng Ling. All rights reserved.
+//
+
+import Foundation
